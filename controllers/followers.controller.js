33f98d877357909e82c657c6666ec8b68a90da18
AnishKhamkar7 <anishkhamkar7@gmail.com>
2024-07-20 10:00:32 +0530

    controller for follow a user

--- controllers/followers.controller.js
+++ controllers/followers.controller.js
@@ -1,10 +1,59 @@
+import { User } from "../models/user.model.js"
+import { Follow } from "../models/followers.models.js"
 
 
-const followAuser = async(req,res)=>{
-    const User = req.user   
-
-    const {_id } = req.params
-
+const followUser = async(req,res)=>{
+    try {
+        const user = req.user   
     
+        const {_id } = req.params
+    
+        const checkuser = await User.findById(_id)
+        
+        //check user avialability
+
+        if (!checkuser || !user) {
+            return res.status(400).json({
+                message: "User Not found"
+            })
+        }
+
+        //once the user is valid then check if you already have followed the user or not
+
+        const vertifyfollower = await Like.findOne({
+            follower: user._id,
+            page: checkuser._id
+        })
+
+        if(vertifyfollower){
+            return res.status(400).json({
+                message: "You have already Followed this account"
+            })
+        }
+        
+        //save follower 
+
+        const saveFollow = await Follow.create({
+            follower: user._id,
+            page: checkuser._id
+        })
+
+        if (!saveFollow) {
+            return res.status(400).json({
+                message:"Unable to follow this User"
+            })
+        }
+
+        return res.status(200).json({
+            message: "You Followed this User"
+        })   
+
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message
+        })
+    }
 
 }
+
+export { followUser }
