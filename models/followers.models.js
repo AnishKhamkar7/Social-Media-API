75cbf83ca7d00d4670166dc50a4ec5ff1c1485d3
AnishKhamkar7 <anishkhamkar7@gmail.com>
2024-07-24 09:25:21 +0530

    Debug

--- models/followers.models.js
+++ models/followers.models.js
@@ -3,11 +3,11 @@ import mongoose from "mongoose";
 const FollowerSchema  = new mongoose.Schema(
     {
         follower: {
-            type: Schema.Types.ObjectId, //One who is the follower
+            type: mongoose.Schema.Types.ObjectId, //One who is the follower
             ref: "User"
         },
         page:{
-            type: Schema.Types.ObjectId, //One to whom "follower" is follwoing
+            type: mongoose.Schema.Types.ObjectId, //One to whom "follower" is follwoing
             ref: "User"
         }
 
